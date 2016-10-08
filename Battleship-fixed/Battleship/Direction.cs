da680b178e87e41fd3a20ad0e1d9ed97bb79e282
100061984george <100061984@students.swinburne.edu.my>
2016-10-08 17:38:08 +0800

    add fixed battleship code files

---
+++ Battleship-fixed/Battleship/Direction.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
+public enum Direction
+{
+	/// <summary>
+	/// The ship is oriented left/right
+	/// </summary>
+	LeftRight,
+
+	/// <summary>
+	/// The ship is oriented up/down
+	/// </summary>
+	UpDown
+}
+
+
---
+++ Battleship-fixed/Battleship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Battleship
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
